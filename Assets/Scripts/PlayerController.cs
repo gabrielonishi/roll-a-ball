ac86737eaf70db1c1e3767c654fa528b3817844f
gabrielonishi <gabrielonishi@outlook.com>
2023-09-12 19:34:41 -0300

    Implementa primeira versão de mecânica de tempo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using System;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
     public TextMeshProUGUI countText;
+    private float timer = 5;
+    public TextMeshProUGUI timerText;
     public GameObject winTextObject;
+    public GameObject loseTextObject;
     private int count;
 
     private Rigidbody rb;
@@ -22,14 +26,30 @@ public class PlayerController : MonoBehaviour
         count = 0;
         setCountText();
         winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer >= 0){
+            int minutes = Mathf.FloorToInt(timer / 60);
+            int seconds = (int) timer % 60;
+            timerText.text = "Timer: " + minutes.ToString() + ":" + seconds.ToString("00");
+            countText.text = "Count: " + count.ToString();
+        }
+        else{
+            loseTextObject.SetActive(true);
+        }
     }
 
     private void OnMove(InputValue movementValue)
     {
-        Vector2 movementVector = movementValue.Get<Vector2>();
-
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+        if(timer >= 0){
+            Vector2 movementVector = movementValue.Get<Vector2>();
+            movementX = movementVector.x;
+            movementY = movementVector.y;
+        }
     }
 
     private void FixedUpdate()
